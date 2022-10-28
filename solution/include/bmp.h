2b2d0a9bb64e64802f9c5d3d45051e21a41e6a6b
Кошелюк Демид <336672@niuitmo.ru>
2022-10-28 11:58:22 +0000

    Update bmp.h

--- solution/include/bmp.h
+++ solution/include/bmp.h
@@ -2,6 +2,5 @@
 #include <stdio.h>
 #include "struct_image.h"
 #include <stdlib.h>
-
-struct image readfile(const char* fileName);
+struct image* readfile(const char* fileName);
 void writefile(const char* fileName, const struct image* image);
