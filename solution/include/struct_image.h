7fb3cfc0ff1d7788815163354517797c8910aefd
Кошелюк Демид <336672@niuitmo.ru>
2022-10-28 11:33:36 +0000

    Upload New File

---
+++ solution/include/struct_image.h
@@ -0,0 +1,15 @@
+#ifndef IMAGE_HEADER_FILE
+#define IMAGE_HEADER_FILE
+#include <stdint.h>
+#pragma pack(push, 1)
+struct pixel {
+    uint8_t b, g, r;
+};
+#pragma pack(pop)
+
+struct image {
+    uint64_t width, height;
+    struct pixel* data;
+};
+struct image create();
+#endif
