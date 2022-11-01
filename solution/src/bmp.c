1aebee7bc7c3cd5765a919716517234a604b0fea
Кошелюк Демид <336672@niuitmo.ru>
2022-11-01 20:24:12 +0000

    Update bmp.c

--- solution/src/bmp.c
+++ solution/src/bmp.c
@@ -26,7 +26,6 @@ enum read_status  {
     READ_INVALID_SIGNATURE,
     READ_INVALID_BITS,
     READ_INVALID_HEADER
-    /* коды других ошибок  */
 };
 
 enum read_status from_bmp( FILE* in, struct image* img ){
@@ -109,7 +108,6 @@ void readfile(const char* fileName, struct image* img){
 enum  write_status  {
     WRITE_OK = 0,
     WRITE_ERROR
-    /* коды других ошибок  */
 };
 
 enum write_status to_bmp( FILE* out, const struct image* img ){
