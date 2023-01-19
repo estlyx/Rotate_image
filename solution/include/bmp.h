6ec40d476c92ace8d6d80b115c22ae6767451552
estlyx <koshelyuk-03@mail.ru>
2023-01-19 15:59:37 +0300

    fixed_4

--- solution/include/bmp.h
+++ solution/include/bmp.h
@@ -1,6 +1,6 @@
+#include "struct_image.h"
 #include  <stdint.h>
 #include <stdio.h>
-#include "struct_image.h"
 #include <stdlib.h>
-void read_file(const char* fileName, struct image*);
+void read_file(const char* fileName, struct image* img);
 void write_file(const char* fileName, const struct image* image);
